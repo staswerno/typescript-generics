83b1ad4bfbe9bee15bde2df8be81852a21385ee4
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-04-12 16:29:26 +0200

    create generic function

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,19 @@ const names: Array<string> = ["Stasi", "Jopo"];
 const promise: Promise<string> = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve("This is done!");
+		reject("Not done.");
 	}, 2000);
 });
 // Promise<any> would give less TS type safety
 // eg if resolve is integer, can use string methods on it
+
+function merge<T, U>(objA: T, objB: U) {
+	return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Stasi" }, { age: 21 });
+// mergedObj.name; doesn't work without <generic> because ts doesn't know
+// type: 'object' too unspecific
+// could typecast but very cumbersome
+// so use <generic> to return intersection of any 2 types
+// set dynamically when calling function
